70693f5266ad8b6f140b717e282f80320e012e8e
isaacwatson30 <isaacwatson30@gmail.com>
2025-12-21 16:22:51 -0600

    completed creation of first adventurer, adding second adventurer and actions

--- src/games/game-1.js
+++ src/games/game-1.js
@@ -1,5 +1,16 @@
+import { Adventurer } from "../models/Adventurer.js";
+
 console.log("Fantasy Forge initialized!");
 // src/games/game-1.mjs
 
-export {};
+// Create a hero instance
+const hero1 = new Adventurer("Robin", 80);
+const hero2 = new Adventurer("Hattie",90)
+console.log("---First Adventurer Created---");
 
+
+// Call the scout method
+hero1.scout();
+hero2.scout();
+
+export {};
--- src/models/Adventurer.js
+++ src/models/Adventurer.js
@@ -4,10 +4,21 @@ class Adventurer {
   constructor(name, health) {
     this.name = name;
     this.health = health;
+    
+  }
+
+  receiveDamage(amount) {
+    this.health -= amount;
+    console.log(`${this.name} received ${amount} damage. Current health: ${this.health}`);
+  }
+
+  heal(amount) {
+    this.health += amount;
+    console.log(`${this.name} healed ${amount} health. Current health: ${this.health}`);
   }
 
   scout() {
     console.log(`${this.name} is scouting... Current health: ${this.health}`);
   }
 }
-export default Adventurer;
+export { Adventurer };
